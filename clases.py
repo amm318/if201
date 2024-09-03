74fb017aed28a2dc11f3ec9679ce30bab309ab10
amm318 <202410472@urp.edu.pe>
2024-09-03 18:25:51 +0000

    Actualizado 03/09/2024

---
+++ clases.py
@@ -0,0 +1,11 @@
+#Clases a usar en nuestros proyectos
+#FECHA: 02/09/2024
+class Persona:
+  def__init__(self, *args):
+    if len(args) ==0:
+            self.nombre = "<sin nombre"
+            self.peso = 0
+    else:
+            self.nombre = args[0]
+            self.peso = aegs[1]
+
